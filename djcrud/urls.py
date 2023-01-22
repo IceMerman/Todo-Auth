a2e919844d2e841632bc5405e9350c79667635d6
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-01-22 18:45:35 -0500

    Added tempaltes and views for signup

--- djcrud/urls.py
+++ djcrud/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tasks import views
+from tasks import urls as tasks_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello_world)
+    path('', include('tasks.urls'))
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.db import IntegrityError
 
 # Create your views here.
 
-def hello_world(request):
+def home(request):
     context = {}
+    return render(request, template_name='home.html', context=context)
+
+def signup(request):
+    
+    context = {}
+    if request.method=="POST":
+        newUser = UserCreationForm(data=request.POST)
+        if newUser.is_valid():
+            newUser.save()
+            return redirect('home')
+        else:
+            context.update(newUser.error_messages)
+
+            # if request.POST['password1'] == request.POST['password2']:
+            #     try:
+            #         user = User.objects.create_user(username=request.POST['username'], password=request.POST['password2'])
+            #         user.save()
+            #     except IntegrityError:
+            #         context['error'] = 'Error user already exists'
+
+            #     except Exception as e:
+            #         return HttpResponse(f'Error {e}')
+            # else:
+                # context['error'] = 'No coinciden las constraseñas'
+        return render(request, template_name='signup.html', context=context)
+        
+    elif request.method=="GET":
+        context = {
+            'form': UserCreationForm
+        }
     return render(request, template_name='signup.html', context=context)
