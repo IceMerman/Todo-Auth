d75382eced7ff95d20c24110896d9196e82f6934
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-01-22 18:55:24 -0500

    Added navigtion file, removed old code for signup

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import redirect
-from django.db import IntegrityError
 
 # Create your views here.
 
