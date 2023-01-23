6eef7b9cbbbff556ffc0c68c2fecc90f6e2ab26d
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-01-23 18:08:32 -0500

    Added taks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    descrption = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    # user = models.ForeignKey('django.contrib.auth.models.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
