97479a612955ee7a47944703027b592417239a9b
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-01-23 20:02:30 -0500

    Autoformmated

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
