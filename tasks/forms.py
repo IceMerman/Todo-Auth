ab9509362bfa62626e4936917cc9731124217b94
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-01-25 21:58:58 -0500

    added django html styles

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea, CheckboxInput
 from .models import Task
 
 
@@ -6,3 +6,8 @@ class createTaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
+            'description': Textarea(attrs={'class': 'form-control', 'placeholder': 'Write the task description'}),
+            'important': CheckboxInput(attrs={'class': 'form-check-input m-auto', 'placeholder': 'Is it important?'})
+        }
