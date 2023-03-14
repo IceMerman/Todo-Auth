630953668626611c75362c2e1b3af0c656715c25
Juan Esteban Sierra 50443 <jsierra@xm.co.co>
2023-03-13 19:05:35 -0500

    Removed aditional info

--- djcrud/settings.py
+++ djcrud/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from os import getenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a^*ug%f9yg!8b9*q=ig6dl8=d-diz#(2n$!l#gc=s52mdm#59w'
+SECRET_KEY = getenv('SECRET_DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
